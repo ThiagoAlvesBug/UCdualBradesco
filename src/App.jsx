eb2880e89a7d886fdcb08eda11a4f78bf1b3e7da
Thiago Alves Bugueño <133525166+ThiagoAlvesBug@users.noreply.github.com>
2025-11-06 12:46:34 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex justify-center h-screen w-scree bg-[#090040]">
+      <div className="w-full h-15 bg-[#040020]
+      text-[#FF0066] flex justify-between p-4 
+      items-center rounded-lg shadow-lg">
+        <h1 className="text-4xl font-bold">Banco Maneiro</h1>
+        <button className="flex bg-[#FF0066] text-white 
+        px-8 py-2 rounded ml-4">Entrar</button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
+
